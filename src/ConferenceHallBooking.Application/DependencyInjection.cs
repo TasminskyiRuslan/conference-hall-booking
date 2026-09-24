6be59363bfdf113c54cd2c6c35aba19f5ebc855d
TasminskyiRuslan <tasminskyi.ruslan@gmail.com>
2026-09-24 18:32:44 +0300

    feat: register application services

--- src/ConferenceHallBooking.Application/DependencyInjection.cs
+++ src/ConferenceHallBooking.Application/DependencyInjection.cs
@@ -1,9 +1,24 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+using ConferenceHallBooking.Application.Behaviours;
+using ConferenceHallBooking.Application.Interfaces.Bookings;
+using ConferenceHallBooking.Application.Services.Bookings;
+using FluentValidation;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(ApplicationServiceExtensions).Assembly));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(typeof(ApplicationServiceExtensions).Assembly);
+
+        services.AddScoped<IPricingService, PricingService>();
+
         return services;
     }
 }
