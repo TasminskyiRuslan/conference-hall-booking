bba1311acac304ff26c60f30d435b803f7b85a79
TasminskyiRuslan <tasminskyi.ruslan@gmail.com>
2026-09-25 10:39:09 +0300

    feat: add bookings no-overlap exclusion constraint

--- src/ConferenceHallBooking.Infrastructure/Migrations/20260925073717_AddBookingsNoOverlapExclusion.Designer.cs
+++ src/ConferenceHallBooking.Infrastructure/Migrations/20260925073717_AddBookingsNoOverlapExclusion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ConferenceHallBooking.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConferenceHallBooking.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260925073717_AddBookingsNoOverlapExclusion")]
+    partial class AddBookingsNoOverlapExclusion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ConferenceHallBooking.Infrastructure/Migrations/20260925073717_AddBookingsNoOverlapExclusion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConferenceHallBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingsNoOverlapExclusion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS btree_gist;");
+
+            migrationBuilder.Sql("""
+                ALTER TABLE "Bookings" ADD CONSTRAINT "Bookings_NoOverlap"
+                EXCLUDE USING gist (
+                    "HallId" WITH =,
+                    tstzrange("StartTime", "EndTime", '[)') WITH &&
+                );
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""ALTER TABLE "Bookings" DROP CONSTRAINT IF EXISTS "Bookings_NoOverlap";""");
+        }
+    }
+}
